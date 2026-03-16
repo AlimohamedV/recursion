0138dfd385aeefd9805880a67f46c19ff0642531
AlimohamedV <102331860+AlimohamedV@users.noreply.github.com>
2026-03-16 05:43:52 +0200

    Update D_Print_Digits_using_Recursion.cpp

--- D_Print_Digits_using_Recursion.cpp
+++ D_Print_Digits_using_Recursion.cpp
@@ -1,14 +1,3 @@
-/*Given a number N. Print the digits of N separated by a space.
-
-Note: Solve this problem using recursion.
-Input
-
-First line contains a number T (1 ≤ T ≤ 10) number of test cases.
-
-Next T lines will contain a number N (0 ≤ N ≤ 109).
-Output
-
-For each test case print a single line contains the digits of the number separated by space.*/
 #include <iostream>
 using namespace std;
 void print(int n) {
